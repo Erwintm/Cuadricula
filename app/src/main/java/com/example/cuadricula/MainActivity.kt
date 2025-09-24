9551aec38351eed2e13e04a55785e7f9dc45b939
Erwin <tapiaerwim@gmail.com>
2025-09-24 17:50:33 -0600

    Cuadricula de cursos

--- app/src/main/java/com/example/cuadricula/MainActivity.kt
+++ app/src/main/java/com/example/cuadricula/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -20,6 +21,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -39,19 +41,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CuadriculaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val exampleTopic = Topic(
-                        name = R.string.photography,
-                        availableCourses = 58,
-                        imageRes = R.drawable.photography
-                    )
-
-
-                    TopicCard(
-                        topic = exampleTopic,
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .padding(16.dp)
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    com.example.cuadricula.TopicGrid(
+                        modifier = Modifier.padding(
+                            start = dimensionResource(R.dimen.padding_small),
+                            top = dimensionResource(R.dimen.padding_small),
+                            end = dimensionResource(R.dimen.padding_small),
+                        )
                     )
                 }
             }
